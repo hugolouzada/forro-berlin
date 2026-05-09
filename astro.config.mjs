813966b9e9a1c9f2868866cad382ff08028d8be3
Vitor Louzada <hugolouzada@gmail.com>
2026-05-09 19:02:56 +0200

    deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://forro-berlin.de',
+  site: 'https://hugolouzada.github.io',
+  base: '/forro-berlin',
 });
